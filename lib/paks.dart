d601ea307610ad323505bccd833bcf0c87c71760
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2021-03-31 17:17:51 -0700

    fix deleting bug

--- lib/paks.dart
+++ lib/paks.dart
@@ -5,50 +5,61 @@ import 'package:PakRat/widgets/pakModal.dart';
 import 'package:PakRat/widgets/addPakItemModal.dart';
 import 'package:PakRat/pakData.dart';
 
-// Learned about FutureBuilder here: https://flutterigniter.com/build-widget-with-async-method-call/
+class Paks extends StatefulWidget {
+  @override
+  PaksState createState() {
+    return PaksState();
+  }
+}
 
-class Paks extends StatelessWidget {
-  String text = "";
-  String desc = "";
-  // List<Widget> children = <Widget>[];
-  var data;
+class PaksState extends State<Paks> {
+  late Future<PakData> pakData;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // initial load
+    pakData = getPak("TEST");
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<PakData>(
-        future: getPak("TEST"),
-        builder: (contect, data) {
-          if (data.data == null)
-            // return Image(image: AssetImage('img\Pakrat_White.png'));
+        future: pakData,
+        builder: (context, futureResult) {
+          if (!futureResult.hasData) {
             return CircularProgressIndicator();
-          else {
-            // buildChildren();
+          } else {
             return Scaffold(
               drawer: SideMenu(),
               appBar: AppBar(
-                title: Text('Paks'),
+                title: Text("Paks"),
               ),
               body: Padding(
-                  padding: EdgeInsets.fromLTRB(10, 14, 10, 14),
-                  child: ListView(
-                    children: [
-                      // children list/array
-                      for (final widget in data.data?.dataItems ?? [])
-                        GestureDetector(
-                          child: Dismissible(
-                            key: Key(widget.title),
-                            child: Card(
-                              child: Center(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(widget.title,
-                                        style: TextStyle(fontSize: 36))
-                                  ],
-                                ),
-                              ),
-                            ),
-                            background: Container(
+                padding: EdgeInsets.fromLTRB(10, 14, 10, 14),
+                child: ListView.builder(
+                  itemCount: futureResult.data!.dataItems.length,
+                  itemBuilder: (context, index) {
+                    final item = futureResult.data!.dataItems[index];
+
+                    return GestureDetector(
+                      child: Dismissible(
+                      key: Key(item.title),
+                      onDismissed: (direction) {
+                        setState(() {
+                          futureResult.data!.dataItems.removeAt(index);
+                        });
+
+                        PakData _pakData = futureResult.data!;
+                        _pakData
+                            .removeItem(PakDataItem(item.title, item.value));
+                        setOrUpdatePak(_pakData);
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("${item.title} dismissed")));
+                      },
+                      background: Container(
                               padding: EdgeInsets.only(left: 12),
                               child: Icon(Icons.delete),
                               alignment: Alignment.centerLeft,
@@ -83,33 +94,32 @@ class Paks extends StatelessWidget {
                                     );
                                   });
                             },
-                            direction: DismissDirection.startToEnd,
-                            onDismissed: (dismissDirection) async {
-                              try {
-                                PakDataItem item =
-                                    PakDataItem(widget.title, widget.value);
-                                PakData pakData = data.data?.removeItem(item);
-                                //data.data?.dataItems.remove(item);
-                                await setOrUpdatePak(pakData);
-                                print("hello");
-                              } catch (e) {
-                                print("something went wrong: $e");
-                              }
-                            },
-                          ),
-                          onTap: () {
+                      child: Card(
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(item.title,
+                                        style: TextStyle(fontSize: 36))
+                                  ],
+                                ),
+                              ),
+                            ),
+                    ),
+                    onTap: () {
                             showDialog(
                                 context: context,
                                 builder: (BuildContext context) {
                                   return PakModal(
-                                    widget.title,
-                                    widget.value,
+                                    item.title,
+                                    item.value,
                                   );
                                 });
                           },
-                        ),
-                    ],
-                  )),
+                    );
+                  },
+                ),
+              ),
               floatingActionButton: FloatingActionButton(
                 child: Icon(Icons.add),
                 backgroundColor: HexColor("bbdefb"),
@@ -124,16 +134,5 @@ class Paks extends StatelessWidget {
             );
           }
         });
-  }
-
-  void buildChildren() {
-    // for(widget in data.data) {
-      // ...stuff
-    // }
-  }
-
-  void removeChild() {
-    // pakDataItem.removeItem(item);
-    // children = pakDataItems;
   }
 }
