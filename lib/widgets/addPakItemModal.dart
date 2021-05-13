cab2ae58d8dff34f4b2d786886acd5b88e9c1505
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2021-05-12 19:43:40 -0700

    allow for multiline text

--- lib/widgets/addPakItemModal.dart
+++ lib/widgets/addPakItemModal.dart
@@ -2,6 +2,7 @@ import 'package:PakRat/paks.dart';
 import 'package:PakRat/widgets/pakModal.dart';
 import "package:flutter/material.dart";
 import 'package:PakRat/pakData.dart';
+import 'package:flutter/services.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:PakRat/camera_screen.dart';
 
@@ -48,54 +49,60 @@ class _AddPakItemModalState extends State<AddPakItemModal> {
     return Stack(
       children: [
         Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (imgURL != "")
-                Text(imgURL),       // TODO: change to show image not link
-              if (imgURL == "")
-              IconButton(
-                  icon: Icon(Icons.camera),
-                  onPressed: () {
-                    navigateToCamera(context, pakName);
-                  }),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 14, 10, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "title",
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (imgURL != "")
+                  Image.network(imgURL,
+                    width: 100,
+                    height: 100,),
+                if (imgURL == "")
+                IconButton(
+                    icon: Icon(Icons.camera),
+                    onPressed: () {
+                      navigateToCamera(context, pakName);
+                    }),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 14, 10, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "title",
+                    ),
+                    onChanged: (value) => title = value,
                   ),
-                  onChanged: (value) => title = value,
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "description",
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "description",
+                    ),
+                    onChanged: (value) => desc = value,
                   ),
-                  onChanged: (value) => desc = value,
                 ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: TextButton(
-                  onPressed: () async {
-                    PakDataItem dataItem = PakDataItem(title, [desc], i: imgURL);
-                    PakData data = await getPak(pakName);
-                    data.addItem(dataItem);
-                    await setOrUpdatePak(data);
-                    navigateToPaks(context, pakName);
-                  },
-                  child: Text("ADD",
-                      style:
-                          TextStyle(fontSize: 16, color: HexColor("bbdefb"))),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () async {
+                      PakDataItem dataItem = PakDataItem(title, [desc], i: imgURL);
+                      PakData data = await getPak(pakName);
+                      data.addItem(dataItem);
+                      await setOrUpdatePak(data);
+                      navigateToPaks(context, pakName);
+                    },
+                    child: Text("ADD",
+                        style:
+                            TextStyle(fontSize: 16, color: HexColor("bbdefb"))),
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          )
         ),
       ],
     );
